0071b23ae9d221295f0fd81ac81614ef4e5f84e5
andyli-al <andy.li@missionsky.com>
2018-08-16 01:53:13 +0800

    本地仓库首次提交

---
+++ src/main/java/com/llf/jenkins/controller/LoginServlet.java
@@ -0,0 +1,31 @@
+package com.llf.jenkins.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userName = request.getParameter("userName");
+		String pswd = request.getParameter("pswd");
+		if(userName.equals("linfeng") && pswd.equals("19931027")){
+			request.setAttribute("userName", userName);
+			request.getRequestDispatcher("jsp/main.jsp").forward(request, response);
+		}else{
+			response.sendRedirect("jsp/login.jsp");
+		}
+	}
+
+}
